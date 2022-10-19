c4a266e03f8ea2f6d0bb5287e5e8786b9efcdbb0
enock-momanyi <enockenm@gmail.com>
2022-10-19 14:29:00 +0300

    update create non-empty todo

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'uyjrb18a2b'
+const apiId = '1tg4gx5cfi'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
